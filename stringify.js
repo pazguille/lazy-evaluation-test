1f262aa5720d33ee2adbd899b9da8aba2092da3a
Guille Paz <guille87paz@gmail.com>
2013-11-25 07:28:31 -0300

    Create stringify file

---
+++ stringify.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var fs = require('fs'),
+    libString = '';
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+    if (err) throw err;
+
+    fs.writeFile('stringified.js', 'window.stringified = ' + JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+    });
+});
